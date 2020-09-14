f8174842fe242950f88c932f4953c4593d4f7036
infernalfire72 <julius.tzunami@yahoo.de>
2020-09-14 17:21:04 +0200

    pakistan

--- commands/pakistan.js
+++ commands/pakistan.js
@@ -1,16 +1,13 @@
+const { playSound } = require('./utils/music');
+
 exports.run = async (client, message) => {
 	const url = 'https://cdn.discordapp.com/attachments/647100049288855582/754785694827675778/I_love_Pakistan_neu.mp3';
 	
 	var channel = message.member.voice.channel;
 	
 	if (channel) {
-		const connection = await channel.join()
-		
-		const dispatcher = connection.play(url, {volume: client.volume});
-		dispatcher.on('finish', () => {
-			channel.leave();
-			dispatcher.destroy();
-		})
+		const connection = await channel.join();
+		playSound(client, connection, url);
 	}
 };
 
