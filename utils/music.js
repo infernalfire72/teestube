232fa928d0accaba66fa2fdb996ad7c80c497c7a
infernalfire72 <julius.tzunami@yahoo.de>
2020-09-25 16:17:00 +0200

    fix

--- utils/music.js
+++ utils/music.js
@@ -1,9 +1,10 @@
 function playSound(client, connection, resource) {
-	const dispatcher = connection.play(resource, {volume: client.volume});
+	const dispatcher = connection.play(resource);
 	dispatcher.on('finish', () => {
 		connection.channel.leave();
 		dispatcher.destroy();
-	})
+	});
+	dispatcher.setVolume(client.volume);
 }
 
 module.exports = {
